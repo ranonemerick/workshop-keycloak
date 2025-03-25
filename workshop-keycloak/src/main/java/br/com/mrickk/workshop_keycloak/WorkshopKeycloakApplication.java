ff26b31ef4d904aecf0dfe762860fefd334e1311
Ranon Emerick Campos <ranon@viaapia.com.br>
2025-03-25 15:17:59 -0300

    Project started

---
+++ workshop-keycloak/src/main/java/br/com/mrickk/workshop_keycloak/WorkshopKeycloakApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrickk.workshop_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopKeycloakApplication.class, args);
+	}
+
+}
---
+++ workshop-keycloak/src/test/java/br/com/mrickk/workshop_keycloak/WorkshopKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrickk.workshop_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
